a300c8f3d879b1a19ba49a19384f09beb27843c4
Rodrigo-Vilemen <rodrigo.vilemen@hotmail.com>
2021-08-18 13:16:15 -0300

    add .

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -48,7 +48,7 @@ export function ChallengeBox() {
         </div>
       ) : (
         <div className={styles.challengeNotActive}>
-          <strong>Finalize um ciclo para receber um desafio</strong>
+          <strong>Finalize um ciclo para receber um desafio.</strong>
           <p>
             <img src="icons/level-up.svg" alt="Level up" />Avance de level completando os desafios.
           </p>
